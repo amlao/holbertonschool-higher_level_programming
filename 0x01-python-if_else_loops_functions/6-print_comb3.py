24486c9b078197140dfe5389ad269d9006ae93b9
Anthony la'O <anthonylao@live.com>
2018-04-11 06:27:15 +0000

    output aesthetics

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for i in range(0, 9 + 1):
         if i == 8 and x == 9:
             print("{:d}{:d}".format(i, x))
         else:
-            print("{:d}{:d}".format(i, x), end="")
+            print("{:d}{:d}, ".format(i, x), end="")
