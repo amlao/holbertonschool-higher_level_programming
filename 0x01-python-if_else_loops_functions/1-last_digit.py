1340662db7e13e9867aaf6d0c9355eaf76c6f27c
Anthony la'O <anthonylao@live.com>
2018-04-11 20:24:53 +0000

    Swapped variables

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,10 +5,10 @@ if number < 0:
     last = number % -10
 else:
     last = number % 10
-if number > 5:
+if last > 5:
     print("Last digit of {} is {} and is greater than 5"
           .format(number, last))
-elif number == 0:
+elif last == 0:
     print("Last digit of {} is {} and is 0".format(number, last))
 else:
     print("Last digit of {} is {} and is less than 6 and not 0"
