5977b737d90ce6bf10d56901353407d01c0f77f2
Anthony la'O <anthonylao@live.com>
2018-04-11 06:21:06 +0000

    ASCII

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if chr(i) != 113 or chr(i) != 101:
-    print("{:c}".format(i), end="")
+    if chr(i) != 'q' and chr(i) != 'e':
+        print("{:c}".format(i), end="")
