6ce30fa41fdc5a938659858bd39c09a033e8c3f0
Anthony la'O <anthonylao@live.com>
2018-04-11 04:40:24 +0000

    Second batch

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for i in range(0, 98 + 1):
+    print("{:d} = {}".format(i, hex(i)))
---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 9 + 1):
+    for x in range(i + 1, 10):
+        if i == 8 and x == 9:
+            print("{:d}{:d}".format(i, x))
+        else:
+            print("{:d}{:d}".format(i, x), end="")
