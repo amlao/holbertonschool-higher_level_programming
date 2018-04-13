58d7c9b55abc921e04b16384e6a3bdf27412d720
Anthony la'O <anthonylao@live.com>
2018-04-13 01:06:27 +0000

    whitespace

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys
     sum = 0
-    for x in range (1, len(sys.argv)):
+    for x in range(1, len(sys.argv)):
         sum += int(sys.argv[x])
     print("{}".format(sum))
