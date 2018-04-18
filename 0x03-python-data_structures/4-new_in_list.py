39b55078f9ef298c965b1e80241c075948417b7c
Anthony la'O <anthonylao@live.com>
2018-04-18 04:46:25 +0000

    Added return value and set condition

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,8 +2,8 @@
 def new_in_list(my_list, idx, element):
     new_list = my_list[:]
 
-    if len(new_list) < idx or idx < 0:
-        return
+    if len(new_list) <= idx or idx < 0:
+        return new_list
     else:
         new_list[idx] = element
         return new_list
