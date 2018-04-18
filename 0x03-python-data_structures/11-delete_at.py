85a24fd726b1efa937b16bb474d086af3c6bd294
Anthony la'O <anthonylao@live.com>
2018-04-18 06:38:52 +0000

    Removed for loop

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    for i in my_list:
-        if idx < 0 or idx >= len(my_list):
-            return my_list
-        my_list.remove(idx + 1)
+    if idx < 0 or idx >= len(my_list):
         return my_list
+    my_list.remove(idx + 1)
+    return my_list
