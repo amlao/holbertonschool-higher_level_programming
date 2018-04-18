50bc14c50bc86d920bb16e40e7603668baa92111
Anthony la'O <anthonylao@live.com>
2018-04-18 05:10:50 +0000

    Restructured conditions

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    newlist = my_list
     for i in my_list:
-        if idx < 0:
-            newlist.remove(i)
-        else:
-            return newlist
+        if idx < 0 or idx >= len(my_list):
+            return my_list
+        my_list.remove(idx + 1)
+        return my_list
