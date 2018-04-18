abea7b9f69523c549c696c6e75119f23429ab5ac
Anthony la'O <anthonylao@live.com>
2018-04-18 04:36:36 +0000

    Separated if statement

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
+    if idx < 0:
+        return
+    elif idx >= len(my_list):
         return
     else:
         my_list[idx] = element
