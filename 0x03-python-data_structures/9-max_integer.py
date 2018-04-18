4e708bcd20986aaa6d4b4bcd3126ad62f576fbe2
Anthony la'O <anthonylao@live.com>
2018-04-18 05:20:20 +0000

    Changed helper function name

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def _max(l):
+def bay(l):
     big = l[0]
     for val in l:
         if val > big:
@@ -10,4 +10,4 @@ def max_integer(my_list=[]):
     if my_list == []:
         return None
     else:
-        return _max(my_list)
+        return bay(my_list)
