498387d527b8347f9eee101b85886707efab6c18
Anthony la'O <anthonylao@live.com>
2018-04-18 06:27:44 +0000

    attempt

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
@@ -7,5 +9,32 @@
  */
 int is_palindrome(listint_t **head)
 {
-        
+        int counter = 0;
+        int len = 0;
+        int *new;
+        listint_t *tmp;
+
+        if (!head)
+                return (1);
+        tmp = *head;
+        for (; tmp; len++)
+                tmp = tmp->next;
+        tmp = *head;
+        new = malloc(sizeof(int) * len);
+        while (tmp)
+        {
+                new[counter] = tmp->n;
+                counter++;
+                tmp = tmp->next;
+        }
+        for (counter = 0; counter < len / 2; counter++)
+        {
+                if (new[counter] != new[len - 1 -counter])
+                {
+                        free(new);
+                        return (0);
+                }
+        }
+        free(new);
+        return (1);
 }
