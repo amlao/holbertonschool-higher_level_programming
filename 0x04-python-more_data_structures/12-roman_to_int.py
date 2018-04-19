6c84e69efc0335877b29d7f0a2e661bd008e960c
Anthony la'O <anthonylao@live.com>
2018-04-19 22:39:03 +0000

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    table = {"I":1,
-               "V":5,
-               "X":10,
-               "L": 50,
-               "C ":100,
-               "D": 500,
-               "M": 1000}
+    i = 0
+    j = 0
+    table =(('M',  1000),
+                   ('CM', 900),
+                   ('D',  500),
+                   ('CD', 400),
+                   ('C',  100),
+                   ('XC', 90),
+                   ('L',  50),
+                   ('XL', 40),
+                   ('X',  10),
+                   ('IX', 9),
+                   ('V',  5),
+                   ('IV', 4),
+                  ('I',  1))
+    for key, value in table:
+        while roman_string[j:j+len(key)] == key:
+            i += value
+            j += len(key)
+        return i
