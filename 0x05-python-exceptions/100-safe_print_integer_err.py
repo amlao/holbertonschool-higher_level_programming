e7ea40da0496b83894215fb47757e0b2acbfe1ba
Anthony la'O <anthonylao@live.com>
2018-04-30 20:17:27 +0000

    Iterate through error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,8 +5,8 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{}".format(value))
-    except ValueError:
-        sys.stderr.write("Exception: {}\n".format(value))
+    except ValueError as err:
+        sys.stderr.write("Exception: {}\n".format(err))
         return False
     finally:
         pass
