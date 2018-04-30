526b5899e686effd6e9f5724853f51967eb7c502
Anthony la'O <anthonylao@live.com>
2018-04-30 10:45:57 +0000

    first two advanced questions

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    try:
+        print("{}".format(value))
+    except ValueError:
+        sys.stderr.write("Exception: {}\n".format(value))
+        return False
+    finally:
+        pass
+    return True
