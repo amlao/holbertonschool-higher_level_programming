65f13d09baa873f6079227d04a25a92a26b50d51
Anthony la'O <anthonylao@live.com>
2018-04-30 20:22:30 +0000

    restructure logic

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,9 +5,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{}".format(value))
-    except ValueError as err:
+        return True
+    except (TypeError, ValueError) as err:
         sys.stderr.write("Exception: {}\n".format(err))
         return False
-    finally:
-        pass
-    return True
