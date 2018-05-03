5d3657cf70dd400ec35a1d74f5e4dc20c7af81aa
Anthony la'O <anthonylao@live.com>
2018-05-03 23:17:48 +0000

    Working #5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -7,3 +7,26 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print('#', end="")
+                print()
