a35768b32d825a71d01ad64138c824bb5ffd71f2
Anthony la'O <anthonylao@live.com>
2018-05-09 09:42:04 +0000

    Fixed spacing errors and updated test file

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -10,9 +10,7 @@ def text_indentation(text):
     each of these characters: ".", "?", ":" """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-
     text = text.replace(".", ".\n\n")
     text = text.replace(",", ",\n\n")
     text = text.replace("?", "?\n\n")
-
     print(text)
