c758b1e69726e3018a4857c19ad8fda1c14a3e46
Anthony la'O <anthonylao@live.com>
2018-05-09 09:33:47 +0000

    Restructured logic for #1 and finished #6

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,29 +6,17 @@ A function that divides all elements of a matrix
 
 def matrix_divided(matrix, div):
     """ Returns a new matrix with all elements divided by div """
-
     new_matrix = []
-    inner = []
-
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    if len(matrix[0]) == 0:
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats")
     for row in matrix:
         if not isinstance(row, list):
             raise TypeError(
-                "matrix must be a (list of lists) of integers/floats")
+                "matrix must be a matrix (list of lists) of integers/floats")
         if len(row) != len(matrix[0]):
-            raise TypeError(
-                "Each row of the matrix must have the same size")
-        for count in row:
-            if not isinstance(count, (int, float)):
-                raise TypeError(
-                    "matrix must be a (list of lists) of integer/floats")
-            else:
-                inner.append(round(count / div, 2))
-        new_matrix.append(inner)
+            raise TypeError("Each row of the matrix must have the same size")
+        else:
+            new_matrix.append([round(count / div, 2) for count in row])
     return new_matrix
--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python3
 """
-Unittests for max_integer
+Unittest for max_integer
 """
+
+
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
-def max_integer(list=[]):
-    """ Find the max integer in a list """
+class TestMaxInteger(unittest.TestCase):
+    """ unittest for max_integer """
+
+    def test_str(self):
+        self.assertEqual(max_integer("fantastic"), "fox")
+
+    def test_int(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_fl(self):
+        self.assertEqual(max_integer([1.2, 2.3, 3.4, 4.5]), 4.5)
+
+    def test_empty(self):
+        self.assertEqual(max_integer([]), None)
+
+    def test_neg(self):
+        self.assertEqual(max_integer([-5, -4, -3, -2, -1]), -1)
