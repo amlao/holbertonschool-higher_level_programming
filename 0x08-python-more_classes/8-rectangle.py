47c16dcc5ecdbb1b763b50abad33a3c397043c72
Anthony la'O <anthonylao@live.com>
2018-05-09 22:00:42 +0000

    Restructured logic for bigger def

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -87,7 +87,9 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_2.area() >= rect_1.area():
+        if rect_1.area() == rect_2.area():
             return rect_1.area()
+        elif rect_1.area() > rect_2.area():
+            return rect_1
         else:
-            return rect_2.area()
+            return rect_2
