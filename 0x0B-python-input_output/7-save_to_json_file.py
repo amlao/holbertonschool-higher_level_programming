4df17ff89d52cf3a16936d2bfb1f8e58da875599
Anthony la'O <anthonylao@live.com>
2018-05-16 22:33:01 +0000

    Spelling error and added return

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -3,5 +3,7 @@
 
 from json import dumps
 
-with open(filename, 'w', encoding="UTF8") as file:
-    dump(my_obj, file)
+
+def save_to_json_file(my_obj, filename):
+    with open(filename, 'w', encoding="UTF8") as file:
+        return dumps(my_obj, file)
