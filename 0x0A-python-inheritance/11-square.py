fea979cda03ee9d3fe42db0373eb21b56b338b41
Anthony la'O <anthonylao@live.com>
2018-05-17 17:41:09 +0000

    Moved validator first

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -9,16 +9,18 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
    """ class square """
+
    def __init__(self, size):
       """ init """
-        self.__size = size
-        super().__init__(size, size)
-        self.integer_validator("size", size)
+      self.integer_validator("size", size)
+      self.__size = size
+      super().__init__(size, size)
+
 
     def area(self):
        """ area """
-        return super().area()
+       return super().area()
 
     def __str__(self):
        """ str """
-        return ("[Square] {:d}/{:d}".format(self.__size, self.__size))
+       return ("[Square] {:d}/{:d}".format(self.__size, self.__size))
