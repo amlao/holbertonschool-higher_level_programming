6e1b9c2abf8d623bd83644d095058580ac59b908
Anthony la'O <anthonylao@live.com>
2018-05-17 04:08:32 +0000

    pep8

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -5,5 +5,6 @@ def pascal_triangle(n):
     arr = []
     for x in range(n):
         row = len(arr)
-        arr = [1 if i == 0 or i == row else arr[i-1]+arr[i] for i in range(row+1)]
+        arr = [1 if i == 0 or i == row else arr[i - 1] + arr[i]
+               for i in range(row + 1)]
         yield arr
