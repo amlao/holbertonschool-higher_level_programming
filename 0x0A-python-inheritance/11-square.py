c6aed7e0338f8513848113ce150d3c99acb0c614
Anthony la'O <anthonylao@live.com>
2018-05-17 17:45:09 +0000

    Testing for last checker

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 
-
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-   """ class square """
-
-   def __init__(self, size):
-      """ init """
-      self.integer_validator("size", size)
-      self.__size = size
-      super().__init__(size, size)
-
+    """ class Square """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
 
     def area(self):
-       """ area """
-       return super().area()
+        """ area """
+        return super().area()
 
     def __str__(self):
        """ str """
-       return ("[Square] {:d}/{:d}".format(self.__size, self.__size))
+        return '[Square] {:d}/{:d}'.format(self.__size, self.__size)
