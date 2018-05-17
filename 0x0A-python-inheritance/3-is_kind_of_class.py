aba43448e6b7e2d0ab10f09b6d1978478fb4db02
Anthony la'O <anthonylao@live.com>
2018-05-17 17:54:26 +0000

    pep8 again

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""
-Checks if the object is an instance of a class
-"""
+
+
+""" Checks if the object is an instance of a class """
 
 
 def is_kind_of_class(obj, a_class):
+    """ Returns True or False upon checking """
     return isinstance(obj, a_class)
