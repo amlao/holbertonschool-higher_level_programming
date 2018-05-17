3bd7d634bcf39400bd5c64b58c0e6b6a323bb4a3
Anthony la'O <anthonylao@live.com>
2018-05-17 17:49:24 +0000

    indentation

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -5,6 +5,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """ class Square """
+
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
@@ -15,5 +16,5 @@ class Square(Rectangle):
         return super().area()
 
     def __str__(self):
-       """ str """
+        """ str """
         return '[Square] {:d}/{:d}'.format(self.__size, self.__size)
