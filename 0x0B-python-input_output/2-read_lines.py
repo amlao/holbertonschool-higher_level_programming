46a243cae9560408257fe109f868487e6dd56319
Anthony la'O <anthonylao@live.com>
2018-05-17 06:30:26 +0000

    checked if nb_lines is lower or equal to 0

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def read_lines(filename="", nb_lines=0):
     with open(filename, encoding="UTF8") as file:
-        for line in range(nb_lines):
-            print(file.readline(), end="")
+        if nb_lines <= 0:
+            print(file.read(), end="")
+        else:
+            for line in range(nb_lines):
+                print(file.readline(), end="")
