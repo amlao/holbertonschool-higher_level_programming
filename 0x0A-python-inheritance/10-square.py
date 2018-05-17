fb4fa4130765610111c493749269b858507db8ef
Anthony la'O <anthonylao@live.com>
2018-05-17 17:46:49 +0000

    order of operations for validator

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,9 +9,9 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     def __init__(self, size):
+        self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
-        self.integer_validator("size", size)
 
     def area(self):
         return super().area()
