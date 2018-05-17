5839b36f1f7238eb4835163fa4101c8a16020afa
Anthony la'O <anthonylao@live.com>
2018-05-17 06:28:16 +0000

    Removed if statement and iterate through nb_lines

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def read_lines(filename="", nb_lines=0):
     with open(filename, encoding="UTF8") as file:
-        for line in file:
-            if nb_lines <= 0:
-                break
+        for line in range(nb_lines):
             print(file.readline(), end="")
