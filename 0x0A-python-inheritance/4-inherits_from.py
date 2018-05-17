274e9f9ef01ee93efd7e5db897a78713f8867780
Anthony la'O <anthonylao@live.com>
2018-05-17 17:23:42 +0000

    test subclass

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,7 +6,4 @@ indirectly) from the specified class ; otherwise False
 
 
 def inherits_from(obj, a_class):
-    if issubclass(type(obj), a_class):
-        return True
-    else:
-        return False
+    return issubclass(type(obj), a_class) and type(obj) != a_class
