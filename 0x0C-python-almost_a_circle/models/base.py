5050625464475fd58b69904b9bc1eb06b52964cb
Anthony la'O <anthonylao@live.com>
2018-05-22 22:54:50 +0000

    pep8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,7 +25,7 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
-    
+
     @classmethod
     def save_to_file(cls, list_objs):
         """ writes json representation to a file """
