e779b9b4b10ee76ab00b692729ac7ba9fb31ade3
Anthony la'O <anthonylao@live.com>
2018-07-28 08:36:31 +0000

    pep8

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-ists all State objects that contain the letter a from the database hbtn_0e_6_usa
+lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa
 """
 from model_state import Base, State
 from sqlalchemy import create_engine
