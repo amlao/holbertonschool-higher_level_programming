1b36b38d5dae3bef634c4846b59adb753b35f160
Anthony la'O <anthonylao@live.com>
2018-07-31 20:11:20 +0000

    Changed order

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -sdX POST "$1" "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -sd "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
