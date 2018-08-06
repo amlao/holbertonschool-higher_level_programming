42d22ebe0283022cd89dcc96e0e400b83e0ef47e
Anthony la'O <anthonylao@live.com>
2018-08-06 19:35:23 +0000

    Clean up code and test for checker

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,6 +7,7 @@ import requests
 
 if __name__ == "__main__":
     req = requests.get('https://intranet.hbtn.io/status')
+    rt = req.text
     print("Body response:")
-    print("\t- type: {}", format(type(req.text)))
-    print("\t- content: {}", format(req.text))
+    print("\t- type: {}".format(type(rt)))
+    print("\t- content: {}".format(rt))
