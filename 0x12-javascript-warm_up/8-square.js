92699939c9ab2e9f05ea1efe3aa4903f5d51bb2e
Anthony la'O <anthonylao@live.com>
2018-08-28 18:01:52 +0000

    Camel case

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const num_times = Number(process.argv[2]);
-if (num_times) {
-    for (let i = 0; i < process.argv[2]; i++) {
-	console.log(Array(num_times + 1).join('X'));
-    }
+const numTimes = Number(process.argv[2]);
+if (numTimes) {
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log(Array(numTimes + 1).join('X'));
+  }
 } else {
-    console.log('Missing size');
+  console.log('Missing size');
 }
